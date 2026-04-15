47bc8beb25c375e72ce030821a6a49bfc8d9a4f9
tlsrneogkrry <k01066530458@gmail.com>
2026-04-15 16:03:21 +0900

    [Feat]에니매이션 구성

--- 1_A Lee Eun Sang MidTerm/Assets/Scripts/PlayerController.cs
+++ 1_A Lee Eun Sang MidTerm/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class  PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public Transform groundCheck;
@@ -12,10 +14,15 @@ public class  PlayerController : MonoBehaviour
     private bool isGrounded;
     private float moveInput;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
 
@@ -31,8 +38,35 @@ public class  PlayerController : MonoBehaviour
     {
         Vector2 input = value.Get<Vector2>();
         moveInput = input.x;
+
+        if (moveInput == 0)
+        {
+            GetComponent<Animator>().SetBool("IsMove", false);
+        }
+        else if (moveInput < 0)
+        {
+            GetComponent<Animator>().SetBool("IsMove", true);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (moveInput > 0)
+        {
+            GetComponent<Animator>().SetBool("IsMove", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
+    private void FixedUpdate()
+    {
+        if (rb.linearVelocityY > 0)
+        {
+            GetComponent<Animator>().SetBool("IsUp", true);
+
+        }
+        else if (rb.linearVelocityY < 0)
+        {
+            GetComponent<Animator>().SetBool("IsUp", false);
+        }
+    }
 
     public void OnJump(InputValue value)
     {
@@ -40,8 +74,27 @@ public class  PlayerController : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            GetComponent<Animator>().SetBool("OnAir", !isGrounded);
         }
     }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+        GetComponent<Animator>().SetBool("OnAir", !isGrounded);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+        GetComponent<Animator>().SetTrigger("Land");
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+        GetComponent<Animator>().SetBool("OnAir", !isGrounded);
+    }
+    
 }
 
 
